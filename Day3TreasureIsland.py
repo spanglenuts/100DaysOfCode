a4cfe7df5e6da67d0d0e30610eedfe50e8e63ee2
spanglenuts <77015187+spanglenuts@users.noreply.github.com>
2021-01-06 14:45:50 -0500

    Create Day3TreasureIsland.py

---
+++ Day3TreasureIsland.py
@@ -0,0 +1,51 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+action = input("You are at a crossroads, do you want to go LEFT or RIGHT? ").lower()
+if action == "right":
+  print("you fell in a hole")
+elif action != "left":
+  print("YOU ARE AN IDIOT!")
+else:
+  action = input("You are at a river, wait or swim? ").lower()
+  if action == "swim":
+    print("You suck at swimming")
+  elif action != "wait":
+    print("YOU ARE SO DUMB!!")
+  else:
+    action = input("Which door will you take, Yellow Red or Blue?")
+    if action == "red":
+      print("You burned in hell")
+    elif action == "blue":
+      print("You got eaten by beast")
+    elif action == "yellow":
+      print("YOU FOUND THE PRINCESSSSS")
+    else:
+      print("YOU ARE REALLLLLY DUMB")
+
+
+      
